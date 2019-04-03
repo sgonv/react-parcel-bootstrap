564707116485526ca0adfc90c03f52e8b419b9b7
Sang-gon Lee <sgon.v@navercorp.com>
2019-04-03 17:32:35 +0900

    Initialize server with Koa

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const logger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
+const serve = require('koa-static')
+
+// Set Koa
+const app = new Koa()
+const router  = new Router()
+
+
+app.use(logger())
+app.use(bodyParser())
+app.use(router.routes())
+app.use(serve('dist'))
+app.listen(8000)
